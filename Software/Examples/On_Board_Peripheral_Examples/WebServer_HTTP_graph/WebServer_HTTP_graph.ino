6d8ebd9910f5f81f060c8647fb33d6458d7da36b
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2019-10-05 11:30:24 +0530

    webserver graph added

--- Software/Examples/On_Board_Peripheral_Examples/WebServer_HTTP_graph/WebServer_HTTP_graph.ino
+++ Software/Examples/On_Board_Peripheral_Examples/WebServer_HTTP_graph/WebServer_HTTP_graph.ino
@@ -28,7 +28,7 @@ void handleRoot() {
     </style>\
   </head>\
   <body>\
-    <h1>Hello from ESP32!</h1>\
+    <h1>Hello from IOTA-SYNC!</h1>\
     <p>Uptime: %02d:%02d:%02d</p>\
     <img src=\"/test.svg\" />\
   </body>\
