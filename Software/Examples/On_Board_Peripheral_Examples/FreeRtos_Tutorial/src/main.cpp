5f0bf37ec6b93902594b2f6a15db81dde5bdd8df
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2019-10-06 10:17:36 +0530

    freertos tutorial added

---
+++ Software/Examples/On_Board_Peripheral_Examples/FreeRtos_Tutorial/src/main.cpp
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+void taskOne( void * parameter );
+void taskTwo( void * parameter);
+void setup()
+{
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  delay(1000);
+
+  xTaskCreate(
+      taskOne,   /* Task function. */
+      "TaskOne", /* String with name of task. */
+      10000,     /* Stack size in bytes. */
+      NULL,      /* Parameter passed as input of the task */
+      1,         /* Priority of the task. */
+      NULL);     /* Task handle. */
+
+  xTaskCreate(
+      taskTwo,   /* Task function. */
+      "TaskTwo", /* String with name of task. */
+      10000,     /* Stack size in bytes. */
+      NULL,      /* Parameter passed as input of the task */
+      1,         /* Priority of the task. */
+      NULL);     /* Task handle. */
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
+
+void taskOne( void * parameter )
+{
+ 
+    for( int i = 0;i<10;i++ ){
+ 
+        Serial.println("Hello from task 1");
+        delay(1000);
+    }
+ 
+    Serial.println("Ending task 1");
+    vTaskDelete( NULL );
+ 
+}
+ 
+void taskTwo( void * parameter)
+{
+ 
+    for( int i = 0;i<10;i++ ){
+ 
+        Serial.println("Hello from task 2");
+        delay(1000);
+    }
+    Serial.println("Ending task 2");
+    vTaskDelete( NULL );
+ 
+}
