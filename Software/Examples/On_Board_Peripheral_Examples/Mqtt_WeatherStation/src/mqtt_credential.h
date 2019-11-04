55f8383e83d4c9df73cfac351705790edd4d02dd
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2019-11-04 06:55:38 +0530

    mqtt credentials

---
+++ Software/Examples/On_Board_Peripheral_Examples/Mqtt_WeatherStation/src/mqtt_credential.h
@@ -0,0 +1,14 @@
+#ifndef _MQTT_CREDENTIALS_H
+#define _MQTT_CREDENTIALS_H
+
+#define WIFI_SSID "wifi_name"
+#define WIFI_PASSWORD "wifi_password"
+
+#define broker_address "ip"
+#define broker_port 1883
+#define mqtt_client_username "username"
+#define mqtt_client_password "password"
+#define mqtt_subscribe_topic "sub"
+#define mqtt_publish_topic "pub"
+
+#endif //_MQTT_CREDENTIALS_H`
